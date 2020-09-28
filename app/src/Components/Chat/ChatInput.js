8f51255d339eebe917098eb6baa9e1cf26837c39
exhlim <exhlimm@gmail.com>
2020-09-28 17:14:08 +0800

    Hosted on firebase

--- app/src/Components/Chat/ChatInput.js
+++ app/src/Components/Chat/ChatInput.js
@@ -13,7 +13,7 @@ function ChatInput({channelName, channelId}) {
                 message: input,
                 timestamp: firebase.firestore.FieldValue.serverTimestamp(),
                 user: user.displayName,
-                userImage: user.photoURL,
+                userImage: user.photoURL
             }) 
         }
         setInput("")
